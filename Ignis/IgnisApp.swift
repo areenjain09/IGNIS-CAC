9aca63d1068d5f5f73439708138699f9ef74ff62
Areen Jain <areenmjain@gmail.com>
2025-07-20 13:47:01 -0700

    Initial Commit

---
+++ Ignis/IgnisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgnisApp.swift
+//  Ignis
+//
+//  Created by Areen Jain on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IgnisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
